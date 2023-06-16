25eee87f10fd95ad93ff314babf9bfeb3c5aac15
roudra323 <asirroudra@gmail.com>
2023-06-17 01:49:56 +0600

    added pausable option

--- contracts/Anon.sol
+++ contracts/Anon.sol
@@ -1,38 +1,124 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.19;
+pragma solidity ^0.8.9;
+
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 /**
  * @title ANON
  * @dev A contract for posting and voting on content.
  */
-contract Anon is ERC20 {
-    address private owner;
-    uint256 public postID;
-
-    struct postInfo {
+contract Anon is ERC20, Pausable, Ownable {
+    struct PostInfo {
         uint256 id;
         string post;
         uint256 time;
         bool downVoted;
     }
 
-    mapping(uint256 => postInfo) public postedInfo;
+    uint256 public postID;
+    PostInfo[] public allPost;
+    mapping(uint256 => PostInfo) public postedInfo;
     mapping(uint256 => address) private resInfo;
     mapping(uint256 => address[]) private voters;
     mapping(uint256 => uint256) private postUP;
     mapping(uint256 => uint256) private postDown;
-
-    postInfo[] internal allPost;
     mapping(address => uint256) internal blacklistedAddress;
     mapping(uint256 => mapping(address => bool)) internal isCoinMinted;
 
-    /**
-     * @dev Initializes the contract and sets the contract deployer as the owner.
-     */
     constructor() ERC20("CSE-Token", "CSE") {
-        owner = msg.sender;
-        _mint(owner, 1000 * 10**decimals());
+        _mint(msg.sender, 1000 * 10**decimals());
+    }
+
+    /**
+     * @dev Creates a new post.
+     * @param _post The content of the post.
+     */
+    function post(string calldata _post) external {
+        require(
+            blacklistedAddress[msg.sender] + 259200 <= block.timestamp,
+            "You can post again after 72 hours!"
+        );
+        PostInfo memory tempPost = PostInfo(
+            postID,
+            _post,
+            block.timestamp,
+            false
+        );
+        resInfo[postID] = msg.sender;
+        postedInfo[postID] = tempPost;
+        allPost.push(tempPost);
+        isCoinMinted[postID][resInfo[postID]] = false;
+        postID++;
+    }
+
+    /**
+     * @dev Retrieves all posts.
+     * @return An array of PostInfo structs representing all the posts.
+     */
+    function viewAllPost() external view returns (PostInfo[] memory) {
+        return allPost;
+    }
+
+    /**
+     * @dev Upvotes a post.
+     * @param _id The ID of the post to upvote.
+     */
+    function upVote(uint256 _id) external voterCheck(_id, msg.sender) {
+        require(allPost.length != 0, "No post found containing this id");
+        require(resInfo[_id] != msg.sender, "Cannot upvote your own post");
+        voters[_id].push(msg.sender);
+        postUP[_id]++;
+    }
+
+    /**
+     * @dev Downvotes a post.
+     * @param _id The ID of the post to downvote.
+     */
+    function downVote(uint256 _id) external voterCheck(_id, msg.sender) {
+        require(allPost.length != 0, "No post found containing this id");
+        require(resInfo[_id] != msg.sender, "Cannot downvote your own post");
+        voters[_id].push(msg.sender);
+        postDown[_id]++;
+    }
+
+    /**
+     * @dev Removes a post if it has more downvotes than upvotes in 24 hours.
+     */
+    function removePost() external whenNotPaused {
+        uint256 currentTime = block.timestamp;
+        for (uint256 i = 0; i < allPost.length; i++) {
+            if (postedInfo[i].time + 86400 <= currentTime) {
+                if (shouldDelete(i)) {
+                    postedInfo[i].downVoted = true;
+                    allPost[i].downVoted = true;
+                    blacklistedAddress[resInfo[i]] = block.timestamp;
+                } else if (
+                    isUPFifty(i) && isCoinMinted[i][resInfo[i]] == false
+                ) {
+                    // Mint CSE token to the poster
+                    isCoinMinted[i][resInfo[i]] = true;
+                    mintCoin(resInfo[i], 1 * 10**(decimals() - 2));
+                }
+            }
+        }
+    }
+
+    /**
+     * @dev Pauses all contract functionality.
+     * Can only be called by the contract owner.
+     */
+    function pause() external onlyOwner {
+        _pause();
+    }
+
+    /**
+     * @dev Unpauses the contract, allowing normal functionality.
+     * Can only be called by the contract owner.
+     */
+    function unpause() external onlyOwner {
+        _unpause();
     }
 
     /**
@@ -48,58 +134,6 @@ contract Anon is ERC20 {
             }
         }
         _;
-    }
-
-    /**
-     * @dev Creates a new post.
-     * @param _post The content of the post.
-     */
-    function post(string calldata _post) public {
-        require(
-            blacklistedAddress[msg.sender] + 259200 <= block.timestamp,
-            "You can post again after 72 hours!"
-        );
-        postInfo memory tempPost = postInfo(
-            postID,
-            _post,
-            block.timestamp,
-            false
-        );
-        resInfo[postID] = msg.sender;
-        postedInfo[postID] = tempPost;
-        allPost.push(tempPost);
-        isCoinMinted[postID][resInfo[postID]] == false;
-        postID++;
-    }
-
-    /**
-     * @dev Retrieves all posts.
-     * @return An array of postInfo structs representing all the posts.
-     */
-    function viewAllPost() external view returns (postInfo[] memory) {
-        return allPost;
-    }
-
-    /**
-     * @dev Upvotes a post.
-     * @param _id The ID of the post to upvote.
-     */
-    function upVote(uint256 _id) public voterCheck(_id, msg.sender) {
-        require(allPost.length != 0, "No post found containing this id");
-        require(resInfo[_id] != msg.sender, "Cannot upvote your own post");
-        voters[_id].push(msg.sender);
-        postUP[_id]++;
-    }
-
-    /**
-     * @dev Downvotes a post.
-     * @param _id The ID of the post to downvote.
-     */
-    function downVote(uint256 _id) public voterCheck(_id, msg.sender) {
-        require(allPost.length != 0, "No post found containing this id");
-        require(resInfo[_id] != msg.sender, "Cannot downvote your own post");
-        voters[_id].push(msg.sender);
-        postDown[_id]++;
     }
 
     /**
@@ -126,30 +160,6 @@ contract Anon is ERC20 {
             return true;
         } else {
             return false;
-        }
-    }
-
-    /**
-     * @dev Removes a post if it has more downvotes than upvotes in 24 hours.
-     */
-    function removePost() public {
-        uint256 currentTime = block.timestamp;
-        for (uint256 i = 0; i < allPost.length; i++) {
-            if (postedInfo[i].time + 86400 <= currentTime) {
-                if (shouldDelete(i)) {
-                    postedInfo[i].downVoted = true;
-                    allPost[i].downVoted = true;
-                    blacklistedAddress[resInfo[i]] = block.timestamp;
-                } else if (isUPFifty(i)) {
-                    require(
-                        isCoinMinted[i][resInfo[i]] == false,
-                        "Already minted once!"
-                    );
-                    // Mint CSE token to the poster
-                    isCoinMinted[i][resInfo[i]] = true;
-                    mintCoin(resInfo[i], 1 * 10**(decimals() - 2));
-                }
-            }
         }
     }
 
